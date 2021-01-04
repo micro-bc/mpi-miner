111e25062b93af06b5a189c041553369a22c9bb4
Jakob Kordež <jakobkordez1999@gmail.com>
2021-01-04 16:42:48 +0100

    Minor fix

--- n5.cpp
+++ n5.cpp
@@ -25,7 +25,8 @@ int main(int argc, char** argv) {
     MPI_Comm_size(MPI_COMM_WORLD, &nproc);
     MPI_Comm_rank(MPI_COMM_WORLD, &id);
 
-    cout << "Comm size: " << nproc << '\n';
+    if (id == MASTER)
+        cout << "Comm size: " << nproc << '\n';
 
     u_long nonceStart = UINT64_MAX / nproc * id;
 
